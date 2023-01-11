835e1034ac8340ff48449589fc1525889287aafd
yangdawei <yangdawei@onlinesign.com.cn>
2023-01-11 17:41:25 +0800

    新增 init_trade_log.sh

---
+++ sql/init_trade_log.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MYSQL_HOST="127.0.0.1"
+MYSQL_USER="root"
+MYSQL_PASS="root"
+MYSQL_DB="trade_log"
+
+mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -e "create database ${MYSQL_DB}"
+
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} < create_trade_log.sql
